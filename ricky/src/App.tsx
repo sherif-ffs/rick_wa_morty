5245619453efc9d68453d248f7eefbbef82c6d12
Sherif Elmetwally <sge5@njit.edu>
2020-09-07 16:48:41 -0400

    delete things

---
+++ ricky/src/App.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function App() {
+  return (
+    <React.Fragment>
+      <h1>Rick and Morty</h1>
+      <p>Pick your favorite episode</p>
+    </React.Fragment>
+  )
+}
