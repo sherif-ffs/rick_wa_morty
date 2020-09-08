edaffb0ae1c4c57877e36780da36a25604ac8186
Sherif Elmetwally <sge5@njit.edu>
2020-09-08 19:48:40 -0400

    create interface for episodes

--- ricky/src/App.tsx
+++ ricky/src/App.tsx
@@ -1,7 +1,27 @@
 import React from 'react';
 import {Store} from './Store'
-import { async } from 'q';
 
+interface IEpsiode {
+  externals: {tvrage: number, thetvdb: number, imdb: string}
+  genres: string[]
+  id: 216
+  image: {medium: string, original: string}
+  language: string
+  name: string
+  network: {id: 10, name: "Adult Swim"}
+  officialSite: string
+  premiered: string
+  rating: {average: 9.1}
+  runtime: number
+  schedule: {time: "23:30", days: string[]}
+  status: string
+  summary: string
+  type: string
+  updated: number
+  url: string
+  webChannel: any
+  weight: number
+}
 export default function App():JSX.Element {
   const {state, dispatch} = React.useContext(Store)
 
@@ -23,6 +43,20 @@ export default function App():JSX.Element {
     <React.Fragment>
       <h1>Rick and Morty</h1>
       <p>Pick your favorite episode</p>
+      <section>
+        {state.episodes.map((episode:any) => {
+          return (
+            <section key={episode.id}>
+              <img src={episode.image.medium} alt={`Rick and Morty ${episode.name}`}></img>
+              <div>{episode.name}</div>
+              <section>
+                <p>Season: {episode.season}</p>
+                <p>Number: {episode.number}</p>
+              </section>
+            </section>
+          )
+        })}
+      </section>
     </React.Fragment>
   )
 }
