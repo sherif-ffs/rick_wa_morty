1b2ff675895c0497488d8bfbf4ec977b833e1ce5
Sherif Elmetwally <sge5@njit.edu>
2020-09-08 19:36:26 -0400

    call api

--- ricky/src/App.tsx
+++ ricky/src/App.tsx
@@ -1,11 +1,26 @@
 import React from 'react';
 import {Store} from './Store'
+import { async } from 'q';
 
 export default function App():JSX.Element {
-  const store = React.useContext(Store)
+  const {state, dispatch} = React.useContext(Store)
+
+  React.useEffect(() => {
+    state.episodes.length === 0 && fetchDataAction()
+  })
+
+  const fetchDataAction = async () => {
+    const data = await fetch('https://api.tvmaze.com/singlesearch/shows?q=rick-&-morty&embed=episodes')
+    const dataJSON = await data.json()
+    console.log('dataJSON: ', dataJSON)
+    return dispatch({
+      type: 'FETCH_DATA',
+      payload: dataJSON._embedded.episodes
+    })
+  }
+  console.log('state: ', state)
   return (
     <React.Fragment>
-      {console.log('store: ', store)}
       <h1>Rick and Morty</h1>
       <p>Pick your favorite episode</p>
     </React.Fragment>
--- ricky/src/Store.tsx
+++ ricky/src/Store.tsx
@@ -1,22 +1,33 @@
 import React from 'react'
+import { act } from 'react-dom/test-utils';
 
 interface IState {
     episodes: Array<''>,
     favorites: Array<''>
 }
 
+interface IAction {
+    type: string,
+    payload: any
+}
 const initialState:IState = {
     episodes: [],
     favorites: []
 }
 
-export const Store = React.createContext<IState>(initialState)
+export const Store = React.createContext<IState | any>(initialState)
 
-function reducer() {
-
+function reducer(state:IState, action:IAction) {
+    switch (action.type) {
+        case 'FETCH_DATA':
+            return {...state, episodes: action.payload}
+        default: 
+            return state
+    }
 }
 
 export function StoreProvider(props:any):JSX.Element {
-    return <Store.Provider value={initialState}>{props.children}</Store.Provider>
+    const [state, dispatch] = React.useReducer(reducer, initialState)
+    return <Store.Provider value={{state, dispatch}}>{props.children}</Store.Provider>
 }
 
